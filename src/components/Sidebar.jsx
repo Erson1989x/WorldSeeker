449870b741450d8beee03c9ee24ecd9156034ccc
Erson1989x <erson1989x@gmail.com>
2025-02-15 05:43:17 -0800

    in-progress

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import AppNav from "./AppNav";
+import Logo from "./Logo/Logo";
+import styles from "./Sidebar.module.css";
+import Footer from "./Footer/Footer";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
+
+export default Sidebar;
