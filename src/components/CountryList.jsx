60770d8de15dd4f2fcc06d86b7423ced44c10df1
Erson1989x <erson1989x@gmail.com>
2025-02-16 07:16:06 -0800

    in-progress

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -12,7 +12,7 @@ const CountryList = ({ cities, isLoading }) => {
     );
 
   const countries = cities.reduce((arr, city) => {
-    if (!arr.map((el) => el.country).includes(city.country))
+    if (!arr.map((elem) => elem.country).includes(city.country))
       return [...arr, { country: city.country, emoji: city.emoji }];
     else return arr;
   }, []);
